bcb1b4454195fd46d04869f7822828ef7d18eb26
TebanHB <e_s_t_e_b_a_n-hurtado@hotmail.com>
2025-11-09 11:41:21 -0400

    Agregar configuración inicial del proyecto Condaty Gateway, incluyendo archivos de configuración, estructura de directorios y dependencias en pom.xml.

---
+++ condaty-gateway/src/main/java/com/condaty/condaty_gateway/CondatyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.condaty.condaty_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondatyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondatyGatewayApplication.class, args);
+	}
+
+}
---
+++ condaty-gateway/src/test/java/com/condaty/condaty_gateway/CondatyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condaty.condaty_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondatyGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
