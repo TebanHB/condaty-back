8f0e472c5e336742ad57712b79d4d7ec3db2d5a3
TebanHB <e_s_t_e_b_a_n-hurtado@hotmail.com>
2025-11-09 11:34:55 -0400

    Agregar archivos de configuración y estructura inicial del proyecto Condaty Eureka Server

---
+++ condaty-eureka/src/main/java/com/condaty/condaty_eureka/CondatyEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.condaty.condaty_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondatyEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondatyEurekaApplication.class, args);
+	}
+
+}
